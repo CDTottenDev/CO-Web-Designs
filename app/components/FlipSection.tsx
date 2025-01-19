3996db34bfac7c3feb621ced9caf7816fd1f2e7a
CDTottenDev <cdtottendev@gmail.com>
2025-01-19 11:25:03 -0800

    Landing page Cretiques

--- app/components/FlipSection.tsx
+++ app/components/FlipSection.tsx
@@ -16,9 +16,9 @@ export default function FlipSection() {
   return (
     <button 
       onClick={handleFlip}
-      className="absolute bottom-0 right-0 w-full h-[calc(50vh/3)] rounded-lg 
-      bg-[#C3CA93]/[0.90] flex flex-col items-center justify-center
-      hover:opacity-90 transition-opacity cursor-pointer"
+      className="absolute bottom-0 right-0 w-full h-[calc(50vh/3)] border-2 border-black
+      bg-[#e2eba7] flex flex-col items-center justify-center
+      hover:opacity-90 transition-opacity cursor-pointer overflow-hidden z-10"
     >
       <img src="/assets/icon-flip-more-black.svg" alt="More Icon" className="w-28 h-28" />
       <span className="text-sm p-2">More</span>
--- app/page.tsx
+++ app/page.tsx
@@ -3,98 +3,98 @@ import FlipSection from './components/FlipSection'
 
 export default function Home() {
   return (
-    <div className="h-screen w-full relative">
+    <div className="h-screen w-full flex">
       {/* Left Column */}
-      <div className="absolute left-0 w-1/2 h-full perspective-1000">
-        <div className="flip-container">
-          <div className="flipper">
+      <div className="w-1/2 perspective-1000 m-0 p-0 border-0">
+        <div className="flip-container h-full m-0 p-0 border-0">
+          <div className="flipper h-full m-0 p-0 border-0">
             {/* Front Content */}
-            <div className="front">
-              <Link href="/about" className="absolute top-0 left-0 w-full h-[calc(50vh-15vh)]
-               bg-[#eeb476] flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
+            <div className="front flex flex-col h-full">
+              <Link href="/about" className="flex-[0_0_35vh] bg-[#fdb76c] border-l-2 border-t-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-about-us-black.svg" alt="About Us Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">About</span>
               </Link>
 
-              <Link href="/contact" className="absolute top-[calc(50vh-15vh)] left-0 w-full h-[calc(25vh)]
-               bg-[#EFE9C5] flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
-                <img src="/assets/icon-contact-black.svg" alt="Contact Me Icon" className="w-28 h-28" />
+              <Link href="/contact" className="flex-[0_0_25vh] bg-[#fffadb] border-l-2 border-b-2 border-t-2 border-black
+              flex flex-col items-center justify-center ">
+                <img src="/assets/icon-contact-black.svg" alt="Contact Us Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">Contact Us</span>
               </Link>
 
-              <Link href="/prices" className="absolute top-[calc(50vh+10vh)] left-0 w-full bottom-0
-               bg-[#93a164] flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
+              <Link href="/prices" className="flex-1 bg-[#d3dfae] border-l-2 border-b-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-price-black.svg" alt="Price Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">Price's</span>
               </Link>
+
             </div>
 
             {/* Back Content */}
-            <div className="back">
-              <Link href="/how-we-work" className="absolute top-0 left-0 w-full h-[calc(50vh-15vh)]
-               bg-[#e4c2ad] flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
+            <div className="back flex flex-col h-full">
+              <Link href="/how-we-work" className="flex-[0_0_35vh] bg-[#e4c2ad] border-t-2 border-l-2 border-b-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-back-work-black.svg" alt="How We Work Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">How We Work</span>
               </Link>
 
-              <Link href="/management-services" className="absolute top-[calc(50vh-15vh)] left-0 w-full h-[calc(25vh)]
-               bg-[#d5caaf] flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
+              <Link href="/management-services" className="flex-[0_0_25vh] bg-[#d5caaf] border-l-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-back-management-black.svg" alt="Website Management Icon" className="w-28 h-28" />
-                <span className="text-sm p-2 text-centera">Website Management Service's</span>
+                <span className="text-sm p-2 text-center">Website Management Service's</span>
               </Link>
 
-              <Link href="/health-checker" className="absolute top-[calc(50vh+10vh)] left-0 w-full bottom-[calc(25vh)]
-               bg-[#dfbf84] flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
+              <Link href="/health-checker" className="flex-[0_0_25vh] bg-[#dfbf84] border-l-2 border-t-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-back-health-black.svg" alt="Website Health Checker Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">Website Health Checker</span>
               </Link>
 
-              <Link href="/faq" className="absolute top-[calc(75vh)] left-0 w-full h-[calc(25vh)]
-               bg-[#e4dbcc] flex flex-col items-center justify-center">
-                <img src="/assets/icon-back-faq-black.svg" alt="FAQ Icon" className="w-28 h-28" />
+              <Link href="/faq" className="flex-1 bg-[#e4dbcc] border-l-2 border-b-2 border-t-2 border-black
+              flex flex-col items-center justify-center">
+                <img src="/assets/icon-back-faq-black.svg" alt="FAQ Icon" className="w-20 h-20" />
                 <span className="text-sm p-2">FAQ</span>
               </Link>
-
             </div>
           </div>
         </div>
       </div>
 
       {/* Right Column */}
-      <div className="absolute right-0 w-1/2 h-full perspective-1000">
-        <div className="flip-container">
-          <div className="flipper ">
+      <div className="w-1/2 perspective-1000 m-0 p-0 border-0">
+        <div className="flip-container h-full m-0 p-0 border-0">
+          <div className="flipper h-full m-0 p-0 border-0">
             {/* Front Content */}
-            <div className="front ">
-              <Link href="/services" className="absolute top-0 right-0 w-full h-[50vh] bg-[#d88b48]
-               flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
+            <div className="front flex flex-col h-full">
+              <Link href="/services" className="flex-[0_0_50vh] bg-[#eea450] border-r-2 border-t-2 border-l-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-services-black.svg" alt="Services Icon" className="w-48 h-48" />
                 <span className="text-sm p-2">Service's</span>
               </Link>
 
-              <Link href="/blog" className="absolute top-[50vh] right-0 w-full h-[calc((50vh/3)*2)]
-               bg-[#808d6f] flex flex-col items-center justify-center hover:opacity-90 transition-opacity">
+              <Link href="/blog" className="flex-[0_0_33.35vh] bg-[#becf8d] border-t-2 border-r-2 border-l-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-blog-black.svg" alt="Blog Icon" className="w-24 h-24" />
                 <span className="text-sm p-2">Blog</span>
               </Link>
             </div>
 
             {/* Back Content */}
-            <div className="back">
-              <Link href="/seo" className="absolute top-0 right-0 w-full h-[25vh]
-               bg-[#c0c8c9] flex flex-col items-center justify-center">
+            <div className="back flex flex-col h-full">
+              <Link href="/seo" className="flex-[0_0_25vh] bg-[#c0c8c9] border-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-back-seo-black.svg" alt="SEO Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">SEO</span>
               </Link>
 
-              <Link href="/web-design" className="absolute top-[25vh] right-0 w-full h-[25vh]
-               bg-[#c0c8c9] flex flex-col items-center justify-center">
+              <Link href="/web-design" className="flex-[0_0_25vh] bg-[#c0c8c9] border-l-2 border-r-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-back-design-black.svg" alt="Web Design Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">Web Design</span>
               </Link>
 
-              <Link href="/knowledge-hub" className="absolute top-[50vh] right-0 w-full h-[calc((50vh/3)*2)]
-               bg-[#a6bcb4] flex flex-col items-center justify-center">
+              <Link href="/knowledge-hub" className="flex-[0_0_33.35vh] bg-[#a6bcb4] border-l-2 border-t-2 border-r-2 border-black
+              flex flex-col items-center justify-center">
                 <img src="/assets/icon-back-knowledge-black.svg" alt="Knowledge Hub Icon" className="w-28 h-28" />
                 <span className="text-sm p-2">Knowledge Hub</span>
               </Link>
@@ -107,3 +107,4 @@ export default function Home() {
     </div>
   )
 }
+
